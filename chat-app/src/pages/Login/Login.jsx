529b55e94e61c80d9ed783ed302ed027ea8a9e2e
Itssagarcode <sagarmaithil70@gmail.com>
2024-10-05 01:43:48 +0530

    UI

---
+++ chat-app/src/pages/Login/Login.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import './Login.css'
+import assets from '../../assets/assets'
+const Login = () => {
+    const [currState, setCurrState] = useState('Sign up')
+  return (
+    <div className='login'>
+        <img src={assets.logo_big} alt="" />
+        <form className='login-form'>
+            <h2>{currState}</h2>
+           {currState == "Sign up" ? <input type="text" placeholder='username' className='form-input' required /> : null} 
+            <input type="email" placeholder=' Email address' className='form-input' required />
+            <input type="password" placeholder='Password' className='form-input' required/>
+            <button type=' submit'>{currState == "Sign up" ? "Create account" : "Login"}</button>
+            <div className="login-term">
+                <input type="checkbox"  />
+                <p>Agree to the terms of use & privacy policy.</p>
+            </div>
+            <div className="login-forget">
+                {
+                    currState == "Sign up"
+                    ?                <p className="login-toggle">Already have a accont <span onClick={()=> setCurrState("Login")} >Login here</span></p>
+                    :                <p className="login-toggle">Create an account <span onClick={()=> setCurrState("Sign up")} >click here</span></p>
+                }
+
+
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Login
