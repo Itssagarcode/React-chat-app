ffaea3392608319c0726e5cc3c51077421a10b65
Itssagarcode <sagarmaithil70@gmail.com>
2024-10-05 11:04:52 +0530

    ui

--- chat-app/src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ chat-app/src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 const ProfileUpdate = () => {
   return (
-    <div>ProfileUpdate</div>
+    <div className='profile'>
+      <div className="profile">
+       <form >
+        <h3>profile Details
+          <label htmlFor="avatar">
+            <input type="file" id='avatar' accept='.png, jpg, jpeg ' hidden />
+            <img src={assets.avatar_icon} alt="" />
+            upload profile image
+          </label>
+        <input type="text" placeholder='Your name' required />
+        <textarea placeholder='Write profile bio' required></textarea>
+        <button type='submit'>save</button>
+        </h3>
+       </form>
+<img src={assets.logo_icon} alt="" />
+      </div>
+    </div>
   )
 }
 
