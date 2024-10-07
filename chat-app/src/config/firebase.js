e620c467850f63aff923d30ed5df0421b510b88e
Itssagarcode <sagarmaithil70@gmail.com>
2024-10-08 01:44:56 +0530

    auth service

---
+++ chat-app/src/config/firebase.js
@@ -0,0 +1,70 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut} from 'firebase/auth';
+import {doc, getFirestore, setDoc} from 'firebase/firestore';
+import { toast } from "react-toastify";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJmVv12puaqY9Wp4gx-2Cf7TYzaID-Wcc",
+  authDomain: "chat-app-15s.firebaseapp.com",
+  projectId: "chat-app-15s",
+  storageBucket: "chat-app-15s.appspot.com",
+  messagingSenderId: "374805289267",
+  appId: "1:374805289267:web:f3828da871873354548646"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+//create account------------------------
+
+const signup = async (username,email,password) =>{
+    try {
+        const res = await createUserWithEmailAndPassword(auth,email,password)
+        const user = res.user;
+        await setDoc(doc(db,"users",user.uid),{
+            id:user.uid,
+            username:username.toLowerCase(),
+            email,
+            name:"",
+            avatar:"",
+            bio:"Hey, There i am using chat app",
+            lastSeen: Date.now()
+        })
+        await setDoc(doc(db,"chats",user.uid),{
+         chatData:[]
+        })
+    } catch (error) {
+        console.error(error)
+        toast.error(error.code.split('/')[1].split('-').join(" "))
+    }
+}
+
+//---------------------log in------------------
+
+const login = async(email,password)=>{
+    try {
+        await signInWithEmailAndPassword(auth, email, password)
+    } catch (error) {
+        console.error(error)
+        toast.error(error.code.split('/')[1].split('-').join(" "))
+    }
+}
+
+//------------------logout------------
+
+const logout = async ()=>{
+ try {
+    await  signOut(auth);
+ }catch (error) {
+    console.error(error)
+    toast.error(error.code.split('/')[1].split('-').join(" "))
+}
+ 
+   
+}
+
+export {signup, login,logout, auth, db};
--- chat-app/src/pages/Login/Login.jsx
+++ chat-app/src/pages/Login/Login.jsx
@@ -1,16 +1,33 @@
 import React, { useState } from 'react'
 import './Login.css'
 import assets from '../../assets/assets'
+import { signup,login } from '../../config/firebase'
 const Login = () => {
+
     const [currState, setCurrState] = useState('Sign up')
+    const [userNama, setUserName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const onSubmitHandler = (event) =>{
+        event.preventDefault();
+        if (currState === "Sign up") {
+            signup(userNama , email , password);
+        }
+        else{
+            login(email,password);
+        }
+    }
+
+
   return (
     <div className='login'>
         <img src={assets.logo_big} alt="" />
-        <form className='login-form'>
+        <form onSubmit={onSubmitHandler} className='login-form'>
             <h2>{currState}</h2>
-           {currState == "Sign up" ? <input type="text" placeholder='username' className='form-input' required /> : null} 
-            <input type="email" placeholder=' Email address' className='form-input' required />
-            <input type="password" placeholder='Password' className='form-input' required/>
+           {currState == "Sign up" ? <input onChange={(e)=> setUserName(e.target.value)} value={userNama} type="text" placeholder='username' className='form-input' required /> : null} 
+            <input onChange={(e)=> setEmail(e.target.value)} value={email} type="email" placeholder=' Email address' className='form-input' required />
+            <input onChange={(e)=> setPassword(e.target.value)} value={password} type="password" placeholder='Password' className='form-input' required/>
             <button type=' submit'>{currState == "Sign up" ? "Create account" : "Login"}</button>
             <div className="login-term">
                 <input type="checkbox"  />
